93f038c23c79f32c61a487c63d917077434ddf4b
Giovana Ellero <112666623+GikaEllero@users.noreply.github.com>
2024-05-30 17:14:42 -0300

    Correção na classe de teste

--- test/ClienteTest.java
+++ test/ClienteTest.java
@@ -20,8 +20,8 @@ public class ClienteTest {
         Calendar calendar = Calendar.getInstance();
         calendar.set(2002, Calendar.MAY, 28);
 
-        pessoa = new ClientePF(5, calendar.getTime(), "Teste Pessoa", "123.456.789-12", 123456789);
-        empresa = new ClientePJ(7, calendar.getTime(), "Teste Empresa", "123.456.789-12", 123456789);
+        pessoa = new ClientePF(5, calendar.getTime(), "Teste Pessoa", "12345678912", 123456789);
+        empresa = new ClientePJ(7, calendar.getTime(), "Teste Empresa", "1223456300189", 123456789);
         cliente = new ControllerCliente();
     }
     
@@ -33,8 +33,7 @@ public class ClienteTest {
         Cliente result = cliente.getCliente(pessoa.getIdCliente());
         
         // Verificar se os dados do cliente são os esperados
-        assertEquals(pessoa.getNome(), result.getNome());
-        assertEquals(pessoa.getCPF(), ((ClientePF) result).getCPF());
+        assertEquals(pessoa, result);
     }
     
     @Test
@@ -45,7 +44,6 @@ public class ClienteTest {
         Cliente result = cliente.getCliente(empresa.getIdCliente());
         
         // Verificar se os dados do cliente são os esperados
-        assertEquals(empresa.getNome(), result.getNome());
-        assertEquals(empresa.getCnpj(), ((ClientePJ) result).getCnpj());
+        assertEquals(empresa, result);
     }
 }
